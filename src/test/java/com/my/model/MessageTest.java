16770bce35174fc155a5fa1f73821f6dc29c16e2
Viktoryia Hrytsai <Viktoryia_Hrytsai@epam.com>
2018-08-27 11:58:02 +0300

    corrected test for getTextName

--- src/test/java/com/my/model/MessageTest.java
+++ src/test/java/com/my/model/MessageTest.java
@@ -1,4 +1,4 @@
-package com.my;
+package com.my.model;
 
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
@@ -6,20 +6,20 @@ import org.junit.jupiter.api.Test;
 public class MessageTest {
 
     @Test
-    void getText(){
-        Message message=new Message();
+    void getText() {
+        Message message = new Message();
         message.setText("Hello!!!");
         String result = message.getText();
         Assertions.assertEquals("Hello!!!", result);
 
-
     }
+
     @Test
     void getTextName() {
-        Message message=new Message();
+        Message message = new Message();
         message.setName("Serg");
         message.setText("Hello!!!");
-        String result=message.getText()+message.getName();
+        String result = message.getTextName();
         Assertions.assertEquals("Hello!!!Serg", result);
     }
 
