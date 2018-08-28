d8037024ab8f871676d620ffcc6906dfc193f173
Viktoryia Hrytsai <Viktoryia_Hrytsai@epam.com>
2018-08-28 12:25:14 +0300

    added GeneralMessage changed InverseMessage and Message

--- src/main/java/com/my/model/Message.java
+++ src/main/java/com/my/model/Message.java
@@ -1,27 +1,8 @@
 package com.my.model;
 
-public class Message {
+public class Message extends GeneralMessage {
 
-    private String text;
-    private String name;
-
-    public void setText(String text) {
-        this.text = text;
-    }
-
-    public String getText() {
-        return text;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getTextName() {
+   public String getTextName() {
         return text + name;
     }
 
