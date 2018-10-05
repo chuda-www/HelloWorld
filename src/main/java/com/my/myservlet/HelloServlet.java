8d9e91aa836f09aeaef24bf06f8ff80bbe631296
Viktoryia Hrytsai <Viktoryia_Hrytsai@epam.com>
2018-10-05 17:05:43 +0300

    servlet request with header 'X-Name'

--- src/main/java/com/my/myservlet/HelloServlet.java
+++ src/main/java/com/my/myservlet/HelloServlet.java
@@ -11,12 +11,9 @@ public class HelloServlet extends HttpServlet {
 
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        String title = req.getParameter("findname");
-        PrintWriter out = resp.getWriter();
+        String title = req.getHeader("X-Name");
 
-        req.setAttribute("name", req.getParameter("findname"));
-
-//        out.write("hello "+title+" from servlet");
+        req.setAttribute("name", title);
         req.getRequestDispatcher("myfile.jsp").forward(req, resp);
 
     }
