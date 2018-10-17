eb844bb1fcff62ce31654efa494d5c1dcc638a65
Viktoryia Hrytsai <Viktoryia_Hrytsai@epam.com>
2018-10-17 15:42:57 +0300

    fixed version logging to 2.11.1

--- src/main/java/com/my/myservlet/HelloServlet.java
+++ src/main/java/com/my/myservlet/HelloServlet.java
@@ -1,6 +1,7 @@
 package com.my.myservlet;
 
-import org.apache.log4j.Logger;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 
 import java.io.IOException;
 import java.io.PrintWriter;
@@ -10,7 +11,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 public class HelloServlet extends HttpServlet {
-    final static Logger log = Logger.getLogger(HelloServlet.class.getName());
+    final static Logger log = LogManager.getLogger(HelloServlet.class.getName());
 
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
