6b9236a59a4dbae6920615152e1c909252f2a8f5
Viktoryia Hrytsai <Viktoryia_Hrytsai@epam.com>
2018-10-24 12:57:09 +0300

    doGet with Mockito

--- src/test/java/com/my/myservlet/HelloServletTest.java
+++ src/test/java/com/my/myservlet/HelloServletTest.java
@@ -4,7 +4,6 @@ import org.junit.Test;
 import org.mockito.ArgumentCaptor;
 import org.mockito.Captor;
 import org.mockito.Mockito;
-import org.mockito.exceptions.verification.NoInteractionsWanted;
 
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
@@ -26,11 +25,15 @@ public class HelloServletTest extends Mockito {
     final RequestDispatcher dispatcher = mock(RequestDispatcher.class);
 
     @Captor
-    ArgumentCaptor <String> argumentGetParameter = ArgumentCaptor.forClass(String.class);
-    ArgumentCaptor <String> argumentSetAttribute = ArgumentCaptor.forClass(String.class);
+    ArgumentCaptor <String> argPostGetParameter = ArgumentCaptor.forClass(String.class);
+    ArgumentCaptor <String> argPostSetAttribute = ArgumentCaptor.forClass(String.class);
+    ArgumentCaptor <String> argPostDispatcher = ArgumentCaptor.forClass(String.class);
+    ArgumentCaptor <String> argGetDispatcher = ArgumentCaptor.forClass(String.class);
+    ArgumentCaptor <String> argGetSetAttribute = ArgumentCaptor.forClass(String.class);
+
 
     @Test
-    public void testServlet() throws ServletException, IOException {
+    public void testDoPost() throws ServletException, IOException {
 
         when(req.getParameter("username")).thenReturn("myName");
         when(req.getParameter("password")).thenReturn("myPassword");
@@ -42,16 +45,38 @@ public class HelloServletTest extends Mockito {
         verify(req, times(1)).getParameter("username");
         verify(req, times(1)).getParameter("password");
         verify(req, times(1)).getRequestDispatcher("user.jsp");
+        verify(req, times(1)).getRequestDispatcher(argPostDispatcher.capture());
         verify(dispatcher).forward(req, resp);
-        verify(req, times(2)).getParameter(argumentGetParameter.capture());
-        verify(req, times(1)).setAttribute(eq("username"), argumentSetAttribute.capture());
-        verify(req, times(1)).setAttribute(eq("password"), argumentSetAttribute.capture());
+        verify(req, times(2)).getParameter(argPostGetParameter.capture());
+        verify(req, times(1)).setAttribute(eq("username"), argPostSetAttribute.capture());
+        verify(req, times(1)).setAttribute(eq("password"), argPostSetAttribute.capture());
         verifyNoMoreInteractions(req, resp, dispatcher);
 
+        assertEquals("user.jsp", argPostDispatcher.getValue());
         List expectedGetParameter = asList("username", "password");
-        assertEquals(expectedGetParameter, argumentGetParameter.getAllValues());
+        assertEquals(expectedGetParameter, argPostGetParameter.getAllValues());
         List expectedSetAttribute = asList("myName", "myPassword");
-        assertEquals(expectedSetAttribute, argumentSetAttribute.getAllValues());
+        assertEquals(expectedSetAttribute, argPostSetAttribute.getAllValues());
+
+    }
+
+    @Test
+    public void testDoGet() throws ServletException, IOException {
+
+        when(req.getHeader("X-Name")).thenReturn("testName");
+        when(req.getRequestDispatcher("myfile.jsp")).thenReturn(dispatcher);
+
+        helloServlet.doGet(req, resp);
+
+        verify(req, times(1)).getHeader("X-Name");
+        verify(req, times(1)).setAttribute(eq("name"), argGetSetAttribute.capture());
+        verify(req, times(1)).getRequestDispatcher("myfile.jsp");
+        verify(req, times(1)).getRequestDispatcher(argGetDispatcher.capture());
+        verify(dispatcher).forward(req, resp);
+        verifyNoMoreInteractions(req, resp, dispatcher);
+
+        assertEquals("testName", argGetSetAttribute.getValue());
+        assertEquals("myfile.jsp", argGetDispatcher.getValue());
 
     }
 
