980bd30787a31bd5cb1025c1e10397f961b5f202
Viktoryia Hrytsai <Viktoryia_Hrytsai@epam.com>
2018-10-29 13:07:30 +0300

    new app with Spring

---
+++ src/main/java/com/my/myspring/MainApp.java
@@ -0,0 +1,13 @@
+package com.my.myspring;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class MainApp {
+    public static void main(String[] args) {
+        ApplicationContext context = new ClassPathXmlApplicationContext("beans.xml");
+        HelloSpring obj = (HelloSpring) context.getBean("helloWorld");
+        obj.getMessage();
+    }
+}
+
