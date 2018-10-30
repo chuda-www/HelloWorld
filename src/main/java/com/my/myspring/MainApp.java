7078ea2798b2af4d0d2a4128556b7a721f408056
Viktoryia Hrytsai <Viktoryia_Hrytsai@epam.com>
2018-10-30 11:44:17 +0300

    added logger and fixed

--- src/main/java/com/my/myspring/MainApp.java
+++ src/main/java/com/my/myspring/MainApp.java
@@ -1,13 +1,18 @@
 package com.my.myspring;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class MainApp {
     public static void main(String[] args) {
+        final Logger log = LogManager.getLogger(MainApp.class.getName());
+
         ApplicationContext context = new ClassPathXmlApplicationContext("beans.xml");
         HelloSpring obj = (HelloSpring) context.getBean("helloWorld");
-        obj.getMessage();
+        System.out.println("Your message: " + obj.getMessage());
+        log.debug("Your message: " + obj.getMessage());
     }
 }
 
